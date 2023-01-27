239baec7fa3375d935cadea4b6b953596cb0f6cc
git4o <71034923+git4o@users.noreply.github.com>
2023-01-26 18:49:54 -0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,14 @@
+bluetooth.onBluetoothConnected(function () {
+    basic.showIcon(IconNames.Yes)
+})
+input.onButtonPressed(Button.A, function () {
+    keyboard.sendString("Hello")
+})
+bluetooth.startAccelerometerService()
+bluetooth.startButtonService()
+bluetooth.startLEDService()
+mouse.startMouseService()
+keyboard.startKeyboardService()
 basic.forever(function () {
 	
 })
